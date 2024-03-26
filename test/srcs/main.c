7585a21531e1a2c42ffb33a19cdc4052cdc21501
Ruben Rollin <rubenrollin@pc-17.home>
2024-03-26 18:09:45 +0100

    clean

--- test/srcs/main.c
+++ test/srcs/main.c
@@ -3,29 +3,7 @@
 int main()
 {
 	void	*ptr;
-	for (int i = 0; i < 200; i++)
-	{
-		ptr = malloc(i % 100);
-	}
-	
 	ptr = malloc(100);
-	ptr = malloc(130);
-	ptr = malloc(150);
-	ptr = malloc(200);
-	ptr = malloc(1000);
-	ptr = malloc(8000);
-	ptr = malloc(12000);
-	ptr = malloc(16000);
 	ft_printf("ptr = %p\n", ptr);
 	return 0;
 }
-
-/*
-
-malloc(24) block size: 32
-malloc(40) block size: 48
-malloc(56) block size: 64
-malloc(72) block size: 80
-malloc(104) block size: 112
-
-*/
